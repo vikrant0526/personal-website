32b257ef93227165477ef9a7536293721c9c6fed
vikrant0526 <60457380+vikrant0526@users.noreply.github.com>
2024-08-10 19:06:56 +0200

    tailwind setup + hero component

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,ts,html,templ,go}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#000004",
+      },
+    },
+  },
+  plugins: [],
+};
