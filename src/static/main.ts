3f369a9365d38aef1e298c9ed52f4a741e18d89a
vikrant0526 <60457380+vikrant0526@users.noreply.github.com>
2024-09-03 01:40:45 +0200

    Testimonials

--- src/static/main.ts
+++ src/static/main.ts
@@ -1,46 +1,59 @@
 import "./style.css";
+import setupTestimonialsSlider from "./slider";
 
-document.addEventListener("DOMContentLoaded", () => {
-  // For cursor effect
+function main() {
+  addCursorListner();
+  addNavBackgroundListner();
+  setupTestimonialsSlider();
+  updateCopyRightDate();
+}
+
+// For cursor effect
+function addCursorListner() {
   const cursor = document.getElementById("cursor");
-  if (cursor) {
-    const trackMouse = (e: MouseEvent) => {
-      cursor.style.opacity = "1";
-      cursor.style.left = e.pageX + "px";
-      cursor.style.top = e.pageY + "px";
-    };
-    const removeCursor = () => {
-      cursor.style.opacity = "0";
-    };
-    removeCursor();
+  if (!cursor) return;
+  const trackMouse = (e: MouseEvent) => {
+    cursor.style.opacity = "1";
+    cursor.style.left = e.pageX + "px";
+    cursor.style.top = e.pageY + "px";
+  };
+  const removeCursor = () => {
+    cursor.style.opacity = "0";
+  };
+  removeCursor();
 
-    document.addEventListener("mousemove", trackMouse);
-    document.addEventListener("mouseenter", trackMouse);
+  document.addEventListener("mousemove", trackMouse);
+  document.addEventListener("mouseenter", trackMouse);
 
-    document.addEventListener("mouseleave", removeCursor);
-    document.addEventListener("scroll", removeCursor);
-  }
+  document.addEventListener("mouseleave", removeCursor);
+  document.addEventListener("scroll", removeCursor);
+}
 
+// For navigation background change on scroll
+function addNavBackgroundListner() {
   const nav = document.getElementById("nav");
-  if (nav) {
-    const navBackgroundToggle = () => {
-      if (window.scrollY > 200) {
-        nav.classList.remove("backdrop-opacity-0");
-        nav.classList.add("bg-primary/50");
-      } else {
-        nav.classList.add("backdrop-opacity-0");
-        nav.classList.remove("bg-primary/50");
-      }
-    };
-    navBackgroundToggle();
-    document.addEventListener("scroll", navBackgroundToggle);
-  }
+  if (!nav) return;
+  const navBackgroundToggle = () => {
+    if (window.scrollY > 200) {
+      nav.classList.remove("backdrop-opacity-0");
+      nav.classList.add("bg-primary/50");
+    } else {
+      nav.classList.add("backdrop-opacity-0");
+      nav.classList.remove("bg-primary/50");
+    }
+  };
+  navBackgroundToggle();
+  document.addEventListener("scroll", navBackgroundToggle);
+}
 
-  // Update the year in the footer
+// Update the year in the footer
+function updateCopyRightDate() {
   const copyRightDate = document.getElementById("copyright-date");
 
-  if (copyRightDate) {
-    const currentYear = new Date().getFullYear();
-    copyRightDate.innerText = currentYear.toString();
-  }
-});
+  if (!copyRightDate) return;
+
+  const currentYear = new Date().getFullYear();
+  copyRightDate.innerText = currentYear.toString();
+}
+
+document.addEventListener("DOMContentLoaded", main);
---
+++ src/static/slider.ts
@@ -0,0 +1,79 @@
+export default function setupTestimonialsSlider() {
+  const slider = document.getElementById("testimonials");
+
+  if (!slider) return;
+
+  const slides = Array.from(slider.children) as HTMLDivElement[];
+
+  slides.forEach((slide, index) => {
+    slide.style.transform = `translateX(${index * 100}%)`;
+    setTimeout(() => {
+      slide.classList.add(
+        "transition-transform",
+        "duration-500",
+        "ease-in-out",
+      );
+    }, 1000);
+  });
+
+  let currentSlide = 0;
+  function showSlides() {
+    slides.forEach((slide, index) => {
+      slide.style.transform = `translateX(${(index - currentSlide) * 100}%)`;
+      if (currentSlide === index) {
+      }
+    });
+    updateDots();
+  }
+
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlides();
+  }
+
+  function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlides();
+  }
+
+  const next = document.getElementById("next");
+  if (next) next.addEventListener("click", nextSlide);
+
+  const prev = document.getElementById("prev");
+  if (prev) prev.addEventListener("click", prevSlide);
+
+  const dotsContainer = document.getElementById("testimonial-dots");
+  if (!dotsContainer) return;
+
+  dotsContainer.classList.add(
+    "flex",
+    "gap-2",
+    "justify-center",
+    "items-center",
+    "p-4",
+  );
+
+  function updateDots() {
+    if (!dotsContainer) return;
+    const dots = dotsContainer.children;
+    for (let i = 0; i < dots.length; i++) {
+      const dot = dots[i] as HTMLButtonElement;
+      if (i === currentSlide) dot.classList.replace("bg-white/50", "bg-white");
+      else dot.classList.replace("bg-white", "bg-white/50");
+    }
+  }
+
+  slides.forEach((_, index) => {
+    const dot = document.createElement("button");
+    dot.classList.add("dot", "w-2", "h-2", "rounded-full");
+    if (index === 0) dot.classList.add("bg-white");
+    else dot.classList.add("bg-white/50");
+    dot.addEventListener("click", () => {
+      currentSlide = index;
+      showSlides();
+    });
+    dotsContainer.appendChild(dot);
+  });
+
+  setInterval(nextSlide, 5000);
+}
